52d4fe7cea8456a14244021c6f8404b2a2800e81
s5kywa1k3r <hestarium@hotmail.com>
2020-01-19 11:20:30 +0000

    refactor(API/*): docker-compose.yml 파일에서 실행 시 mount 위치가 Dockerfile 상위 폴더에 있어도 무방함을 확인하여 폴더 위치 변경

--- API/a000/product_list/index.ts
+++ API/a000/product_list/index.ts
@@ -1,5 +1,5 @@
-import { scrapComponent } from './src/scrap.component';
 import { ProductListRequest } from '../Common/action';
+import { scrapComponent } from './src/scrap.component';
 
 export const index = async (keyword: ProductListRequest) => {
     return scrapComponent(keyword.search_word).then(value => value);
--- API/a000/product_list/product_list.e2e.ts
+++ API/a000/product_list/product_list.e2e.ts
@@ -1,6 +1,6 @@
 import { index } from '.';
 
-index({ search_word: '모니터' })
+index('모니터')
     .then(value => console.log(value))
     .catch(err => {
         throw new Error(err);
