ad39b845c63d458e2a0b256baf6da6059ab59d19
suzn2 <suzn2@naver.com>
2020-02-03 18:03:28 +0900

    수정

--- Main_Server/API/a005/product_detail/src/detailResponseMapping.ts
+++ Main_Server/API/a005/product_detail/src/detailResponseMapping.ts
@@ -2,9 +2,9 @@ import { DetailResponse } from '../../interfaces/DetailResponse.interface';
 import { ProductDetailResponse } from '../../Common/action/product_detail/ProductDetailResponse.interface';
 
 export const detailResponseMapping = (input: DetailResponse): ProductDetailResponse => ({
-    id: 1,
+    id: Number.parseInt(input.data.linkUrl.split('/')[4]),
     site_code: '005',
-    origin_url: input.data.linkUrl,
+    origin_url: input.data.share.linkUrl,
     title: input.data.title,
     price: input.data.property.price.amount,
     date: input.data.timeago,
@@ -12,5 +12,6 @@ export const detailResponseMapping = (input: DetailResponse): ProductDetailRespo
     detail: input.data.description,
     tags: {
         location: input.data.property.location.address,
+        delivery: input.data.property.hasDeliveryFee == true ? '배송비있음' : '무료배송',
     },
 });
