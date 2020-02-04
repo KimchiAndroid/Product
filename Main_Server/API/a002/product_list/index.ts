bd772050e9dc6825d7aba53b47192543e1aa5a0b
dearyeon <d_earyeon@naver.com>
2020-02-04 18:15:26 +0900

    re init

--- Main_Server/API/a002/product_list/index.ts
+++ Main_Server/API/a002/product_list/index.ts
@@ -1,54 +1,51 @@
 import * as rp from 'request-promise';
 import * as cheerio from 'cheerio';
-//import * as iconv from 'iconv-lite';
 
- const siteurl = 'https://m.cafe.naver.com/ArticleAllListAjax.nhn';
- const requestService = async (qs : SiteRequest) => {
-    return rp.get({ url : siteurl, qs : makeqs , json: true, encoding: null});
-  };
-
-(async() => {
-    let ulList = [];
+ const basesiteurl = 'https://m.cafe.naver.com/ArticleSearchList.nhn?';
+ const cafeurl = 'https://m.cafe.naver.com';
+ /*const requestService = async (qs : SiteRequest) => {
+    return rp.get({ url : basesiteurl, qs : makeqs , json: true, encoding: null});
+  };*/
+  const makeqstest= (keyword : string )=> ({
+    'search.query' : keyword, 
+    'search.menuid' : 0,
+    'search.searchBy' : 0,
+    'search.sortBy' : 'date',
+    'search.clubid' : '10050146', //중고나라id
+    'search.option' : 0,
+    'search.defaultValue' : 1,
+  });
+  const scrapComponent = async (keyword : string ) => {
     //const siteurl = await rp("https://m.cafe.naver.com/ArticleAllListAjax.nhn?search.clubid=10050146&search.boardtype=L&search.questionTab=A&search.totalCount=201&search.page=2");
-    //const siteurl = await rp("https://m.cafe.naver.com/ArticleAllListAjax.nhn");
-    const response = await requestService(makeqs);
-    //var rpbuffer = new Buffer(response);
-    //const xml_string = iconv.decode(rpbuffer, 'EUC-KR').toString();
-    console.log(response);
-    console.log(Buffer.from(response).toString());
+    const qs = Object.entries(makeqstest(keyword)).map(e => e.join('=')).join('&');
+    const queryurl = encodeURI(basesiteurl+qs);
+    console.log(queryurl);
 
-    const $ = cheerio.load(response);
-   // rp.get(siteurl, params);
-    const url = $('.list_area .board_box a.txt_area')
-        .map((index, element) => {
-            return $(element).attr("href");
-        })
-        .toArray();
-    //console.log(url);
-
-    const titles = $('.txt_area .tit')
+    const siteurl = await rp(queryurl);
+    const $ = cheerio.load(siteurl);
+    const titles = $('.search_list .tit h3')
         .map((index, element) => {
             return $(element).text();
         })
         .toArray();
-​    //console.log(titles);
-})();
+    console.log(titles);
 
-const makeqs = {
+    const url = $('.search_list .list_tit a')
+        .map((index, element) => {
+            return cafeurl+$(element).attr("href");
+        })
+        .toArray();
+​    console.log(url);
+};
+
+/*const makeqs = {
     'search.clubid' : '10050146',  //중고나라id
     'search.boardtype' : 'L',
     'search.questionTab' : 'A',
     'search.totalCount' : '201',
     'search.page' : 1,
-  };
-/*
-const params = {
-    'search.clubid' : '10050146',  //중고나라id
-    'search.boardtype' : 'L',
-    'search.questionTab' : 'A',
-    'search.totalCount' : '201',
-    'search.page' : 1,
-}*/
+  };*/
+
 interface SiteRequest {
     'search.clubid' : string;
     'search.boardtype': string;
@@ -56,3 +53,16 @@ interface SiteRequest {
     'search.totalCount' : string;
     'search.page' : number;
   }
+
+  interface ProductlistRequest {
+    id: number;
+    site_code : '002';
+    title: string;
+    price: number;
+    thumbnail?: string;
+  }
+
+scrapComponent('모니터')
+.then(value => console.log(value))
+.catch(err => console.log(err))
+.finally(() => process.exit(0));
