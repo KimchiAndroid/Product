cf502d8cea5afbad29e4b61c8fd39667b16dcd67
DoHyeon Jeon <whdies816@gmail.com>
2020-02-05 15:04:09 +0900

    temp commit

--- Main_Server/src/check/index.ts
+++ Main_Server/src/check/index.ts
@@ -1,7 +1,12 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import * as path from 'path';
 import { CheckLinkResponse } from '../../Common';
-var elevenst = require('../../API/dist/a000/check_link');
+import * as a000 from '../../API/a000/check_link';
+// var a001 = require('../../API/dist/a001/check_link');
+// var a002 = require('../../API/dist/a002/check_link');
+// var a003 = require('../../API/dist/a003/check_link');
+// var a004 = require('../../API/dist/a004/check_link');
+// var a005 = require('../../API/dist/a005/check_link');
 
 const checkRouter = Router();
 
@@ -13,16 +18,48 @@ checkRouter.get('/db', async (req: Request, res: Response, next: NextFunction) =
     return res.send('db Success');
 });
 
-checkRouter.get('/11st', async (req: Request, res: Response, next: NextFunction) => {
-    const result: CheckLinkResponse = await elevenst.index();
+checkRouter.get('/a000', async (req: Request, res: Response, next: NextFunction) => {
+    const result: CheckLinkResponse = await a000.index();
     if (result.isSuccess) {
         return res.status(200).send('Successfully connected');
     }
     return res.status(500).send(result.error);
 });
 
-checkRouter.get('/joongna', (req: Request, res: Response, next: NextFunction) => {
-    res.send('joongna Success');
-});
+// checkRouter.get('/a001', async (req: Request, res: Response, next: NextFunction) => {
+//     const result: CheckLinkResponse = await a001.index();
+//     if (result.isSuccess) {
+//         return res.status(200).send('Successfully connected');
+//     }
+//     return res.status(500).send(result.error);
+// });
+// checkRouter.get('/a002', async (req: Request, res: Response, next: NextFunction) => {
+//     const result: CheckLinkResponse = await a002.index();
+//     if (result.isSuccess) {
+//         return res.status(200).send('Successfully connected');
+//     }
+//     return res.status(500).send(result.error);
+// });
+// checkRouter.get('/a003', async (req: Request, res: Response, next: NextFunction) => {
+//     const result: CheckLinkResponse = await a003.index();
+//     if (result.isSuccess) {
+//         return res.status(200).send('Successfully connected');
+//     }
+//     return res.status(500).send(result.error);
+// });
+// checkRouter.get('/a004', async (req: Request, res: Response, next: NextFunction) => {
+//     const result: CheckLinkResponse = await a004.index();
+//     if (result.isSuccess) {
+//         return res.status(200).send('Successfully connected');
+//     }
+//     return res.status(500).send(result.error);
+// });
+// checkRouter.get('/a005', async (req: Request, res: Response, next: NextFunction) => {
+//     const result: CheckLinkResponse = await a005.index();
+//     if (result.isSuccess) {
+//         return res.status(200).send('Successfully connected');
+//     }
+//     return res.status(500).send(result.error);
+// });
 
 export default checkRouter;
