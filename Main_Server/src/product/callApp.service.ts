384daa11e7f08e334ea0922a6d9ff0ea2d06937d
DoHyeon Jeon <whdies816@gmail.com>
2020-02-06 15:38:19 +0900

    Server Update

--- Main_Server/src/product/callApp.service.ts
+++ Main_Server/src/product/callApp.service.ts
@@ -1,32 +1,28 @@
 import { ProductListRequest, ProductListResponse, SiteCode } from '../../Common';
 import { index as a000 } from '../../API/a000/product_list';
+// import { index as a001 } from '../../API/a001/product_list';
+// import { index as a002 } from '../../API/a002/product_list';
+// import { index as a003 } from '../../API/a003/product_list';
+// import { index as a004 } from '../../API/a004/product_list';
 import { index as a005 } from '../../API/a005/product_list';
-// var a001 = require('../../API/dist/a001/product_list');
-// var a002 = require('../../API/dist/a002/product_list');
-// var a003 = require('../../API/dist/a003/product_list');
-// var a004 = require('../../API/dist/a004/product_list');
 
 export const callApp = (data: ProductListRequest) => (
     site_code?: SiteCode,
 ): Array<Promise<ProductListResponse[]>> => {
-    try {
-        switch (site_code) {
-            case '000':
-                return [a000(data)];
-            case '001':
-                return [];
-            case '002':
-                return [];
-            case '003':
-                return [];
-            case '004':
-                return [];
-            case '005':
-                return [a005(data)];
-            default:
-                return [a000(data), a005(data)];
-        }
-    } catch (err) {
-        throw new Error(err);
+    switch (site_code) {
+        case '000':
+            return [a000(data)];
+        case '001':
+            return [];
+        case '002':
+            return [];
+        case '003':
+            return [];
+        case '004':
+            return [];
+        case '005':
+            return [];
+        default:
+            return [a000(data)];
     }
 };
