96dae369c339ff557e18eb03479cf08a5f20938d
yuz413 <yujn413@naver.com>
2020-02-06 17:35:29 +0900

    final_commit

--- Main_Server/API/a001/Common/site_code/SiteCode.type.ts
+++ Main_Server/API/a001/Common/site_code/SiteCode.type.ts
@@ -9,14 +9,14 @@
 export type SiteCode = Elevenst | BeonGae | JoongGo | Daangn | SellIt | Hello;
 
 /** Elevenst 11번가(test 전용) */
-export type Elevenst = '000';
+type Elevenst = '000';
 /** BeonGae : 번개장터 */
-export type BeonGae = '001';
+type BeonGae = '001';
 /** JoonGo : 중고나라 */
-export type JoongGo = '002';
+type JoongGo = '002';
 /** Daangn : 당근마켓 */
-export type Daangn = '003';
+type Daangn = '003';
 /** SellIt : 셀잇 */
-export type SellIt = '004';
+type SellIt = '004';
 /** Hello : 헬로마켓 */
-export type Hello = '005';
+type Hello = '005';
