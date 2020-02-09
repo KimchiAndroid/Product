5c9ba752e372cc8c9ca2e61f6516ae7e3b8b9110
suzn2 <suzn2@naver.com>
2020-02-09 15:07:38 +0900

    날짜포맷 수정

--- Main_Server/API/a005/product_detail/product_detail.e2e.ts
+++ Main_Server/API/a005/product_detail/product_detail.e2e.ts
@@ -1,8 +1,13 @@
 import { index } from '.';
 
-index({ id: '165502697' })
+index({ id: '165531727' })
     .then(value => console.log(value))
     .catch(err => {
         throw new Error(err);
     })
     .finally(() => process.exit(0));
+
+//2020-02-06 : 164839074
+//2020-01-21 : 165395900
+//2019-12-30 : 165148736
+//2020-02-03 : 165531727
--- Main_Server/API/a005/product_detail/src/detailResponseMapping.ts
+++ Main_Server/API/a005/product_detail/src/detailResponseMapping.ts
@@ -7,11 +7,57 @@ export const detailResponseMapping = (input: DetailResponse): ProductDetailRespo
     origin_url: input.data.share.linkUrl,
     title: input.data.title,
     price: input.data.property.price.amount,
-    date: input.data.timeago,
+    //date: input.data.timeago,
+    date: setDate(input.data.timeago),
     image: input.data.images.map(value => value.imageUrl),
     detail: input.data.description,
     tags: {
-        location: input.data.property.location.address,
+        //location: input.data.property.location.address,
         delivery: input.data.property.hasDeliveryFee == true ? '배송비있음' : '무료배송',
     },
 });
+
+const setDate = (input: string) => {
+    //헬로마켓 날짜포맷 '20. 2. 6.'
+    if (input.indexOf('.') !== -1) {
+        const tmp = input.split('. ');
+        if (tmp.length == 2) tmp.unshift('2020'); //헬로마켓 날짜포맷에서 2020년에 올라온 글은 연도가 나타나지 않기 때문에 포맷 맞추기 위해 2020 넣어줌
+        tmp[tmp.length - 1] = tmp[tmp.length - 1].replace('.', '');
+        const today = {
+            year: Number.parseInt(tmp[0]),
+            month: comp(Number.parseInt(tmp[1])),
+            date: comp(Number.parseInt(tmp[2])),
+        };
+        const result = Object.values(today).join('-');
+        return result;
+    }
+    //몇일전, 몇분전, 몇시간전일 경우
+    else {
+        const date = new Date();
+        const minute = parsing('분');
+        const hour = parsing('시간');
+        const day = parsing('일');
+
+        date.getHours() - hour;
+        date.getMinutes() - minute;
+
+        const today = {
+            year: date.getFullYear(),
+            month: comp(date.getMonth() + 1),
+            date: comp(date.getDate() - day), //몇일전일 경우 숫자로 바꿔서 오늘 날짜에서 빼줌
+        };
+        const result = Object.values(today).join('-');
+        return result;
+    }
+};
+
+//월,일을 두자리수로 표현하기 위한 함수
+const comp = (date: number) => {
+    const result = date < 10 ? '0' + date : date;
+    return result;
+};
+
+const parsing = (input: string) => {
+    const result = input.indexOf(input) !== -1 ? Number.parseInt(input.split(input)[0]) : 0;
+    return result;
+};
