acbc844c786dcd815e90f6b437a4b3110ae8f1fd
DoHyeon Jeon <whdies816@gmail.com>
2020-02-19 23:57:26 +0900

    a002 product list

--- Main_Server/API/a002/product_list/src/scrap.component.ts
+++ Main_Server/API/a002/product_list/src/scrap.component.ts
@@ -3,22 +3,45 @@ import { ProductListRequest, ProductListResponse } from '../../Common';
 import { SiteListRequest } from '../../interfaces/SiteRequest.interface';
 import { APISite } from '../../option';
 import { responseMapping } from './responseMapping';
+import { SiteListResponse } from '../../interfaces/SiteResponse.interface';
 
-const scrapComponent = (search_word: string, page: string) => {
-    const options = {
-        body: JSON.stringify(makejson(search_word, page)),
-        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
-    };
-    return rp.post(APISite, options);
+const scrapComponent = async (search_word: string, page: string): Promise<string> => {
+    return rp.post(APISite, requestOption(search_word, page));
+};
+
+const requestOption = (search_word: string, page: string) => ({
+    body: JSON.stringify(makejson(search_word, page)),
+    headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+});
+
+const scrapAllComponent = async (search_word: string): Promise<string[]> => {
+    const result = await rp.post(APISite, requestOption(search_word, '1'));
+    const tempResult: { data: SiteListResponse[] } = JSON.parse(result);
+    const count = tempResult.data[0].totalCnt < 10000 ? tempResult.data[0].totalCnt : 10000;
+    const count_array = Array(Math.floor(count / 60)).fill('');
+
+    const requestArray = count_array.map((_, index) => {
+        return rp.post(APISite, requestOption(search_word, String(index + 1)));
+    });
+    return Promise.all(requestArray);
 };
 
 export const mapping = async ({ search_word, page }: ProductListRequest) => {
-    return scrapComponent(search_word, page).then(value => {
+    const mappingFunction = (value: string) => {
         const response = JSON.parse(value);
-        const mappingArray: ProductListResponse[] = response.data.map(function(element: any) {
-            return responseMapping(element);
-        });
+        const mappingArray: ProductListResponse[] = response.data.map((element: SiteListResponse) =>
+            responseMapping(element),
+        );
         return mappingArray;
+    };
+    if (page !== '') {
+        return scrapComponent(search_word, page).then(mappingFunction);
+    }
+    return scrapAllComponent(search_word).then(value_list => {
+        return value_list.reduce((acc: ProductListResponse[], cur) => {
+            acc = acc.concat(mappingFunction(cur));
+            return acc;
+        }, []);
     });
 };
 
@@ -42,8 +65,8 @@ const makejson = (keyword: string, pageNum: string): SiteListRequest => ({
         productSectionType: 0,
     },
     isSearchSaved: 0,
-    searchQuantity: Number(pageNum) * 60,
+    searchQuantity: 60,
     searchWord: keyword,
     sort: { date: 0, order: 0, price: 0 },
-    startIndex: (Number(pageNum) - 1) * 60,
+    startIndex: parseInt(pageNum, 10) - 1,
 });
