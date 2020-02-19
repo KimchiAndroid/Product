e43c5f7abae78b2f300ad4a38d5ddada5c8e7622
DoHyeon Jeon <whdies816@gmail.com>
2020-02-19 18:08:17 +0900

    a004 check connection

--- Main_Server/API/a004/check_connection/src/request.service.ts
+++ Main_Server/API/a004/check_connection/src/request.service.ts
@@ -1,9 +1,20 @@
-import * as request from 'request';
-import { APISite } from '../../option';
-//요청에 대한 응답
-export const requestService = () => {
-    request.get(APISite, (err, res) => {
-        if (err) console.log(err);
-        console.log(res.body);
+import * as request from 'request-promise';
+import { CheckLinkResponse } from '../../Common/action';
+import { response } from 'express';
+
+export const requestService = async () => {
+    let result: CheckLinkResponse = {
+        isSuccess: false,
+        error: null,
+    };
+    await request('https://www.withsellit.com/api/v1/goods?ids%5B%5D=631704', (error, res) => {
+        if (error) {
+            result.error = error;
+        }
+        if (response.statusCode !== 200) {
+            result = { isSuccess: false, error };
+        }
+        result = { isSuccess: true };
     });
+    return result;
 };
